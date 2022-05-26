743419013cee1e65eba34fe73a48e0fa42a0e4d2
cladg92 <cladg92@gmail.com>
2022-05-26 14:34:39 +0200

    Implement Form react-Bootstrap component

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -1,9 +1,11 @@
-import React, { useState } from 'react';
-import { RegisterView } from '../register-view/register-view';
+import React, { useState } from "react";
+import { RegisterView } from "../register-view/register-view";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
 
 export function LoginView(props) {
-  const [ username, setUsername ] = useState('');
-  const [ password, setPassword ] = useState('');
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -15,22 +17,32 @@ export function LoginView(props) {
 
   const handleNewUser = (e) => {
     e.preventDefault();
-    console.log('register');
-    return <RegisterView />
-  }
+    console.log("register");
+    return <RegisterView />;
+  };
 
   return (
-    <form>
-      <label>
-        Username:
-        <input type="text" value={username} onChange={e => setUsername(e.target.value)} />
-      </label>
-      <label>
-        Password:
-        <input type="password" value={password} onChange={e => setPassword(e.target.value)} />
-      </label>
-      <button type="submit" onClick={handleSubmit}>Submit</button>
-      <button type="submit" onClick={handleNewUser}>Register</button>
-    </form>
+    <Form>
+      <Form.Group controlId="formUsername">
+        <Form.Label>Username:</Form.Label>
+        <Form.Control
+          type="text"
+          onChange={(e) => setUsername(e.target.value)}
+        />
+      </Form.Group>
+      <Form.Group controlId="formPassword">
+        <Form.Label>Password:</Form.Label>
+        <Form.Control
+          type="password"
+          onChange={(e) => setPassword(e.target.value)}
+        />
+      </Form.Group>
+      <Button variant="primary" type="submit" onClick={handleSubmit}>
+        Submit
+      </Button>{" "}
+      <Button variant="secondary" type="submit" onClick={handleNewUser}>
+        Register
+      </Button>
+    </Form>
   );
 }
